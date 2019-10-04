a50677e18453a48dbb7475578727ee65dfb91ccb
newerric <newerric@yahoo.com>
2019-10-04 23:57:24 +0800

    add Day19 for IT30_11, ToDoList (1)

---
+++ ToDoList/ToDoList/ListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ListViewController.swift
+//  ToDoList
+//
+//  Created by mikewang on 2019/10/4.
+//  Copyright © 2019 mikewang. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    @IBOutlet weak var todoTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoList/ToDoList/TodoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoTableViewCell.swift
+//  ToDoList
+//
+//  Created by mikewang on 2019/10/4.
+//  Copyright © 2019 mikewang. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
